2d9e8a18dd5ee90740655d340f9900f14098a02b
smudgeface <jordan.c.paul@gmail.com>
2025-04-05 01:25:44 -0700

    revert some test code

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ async def main():
     # Start servers/tasks
     captive_portal.start_dns_server_task()
     web_server.start_web_server()
-    uart = uart_async.Rmt_Uart(0)
+    uart = uart_async.Hw_Uart(0)
     uart.start()
     extron = ExtronSwVga(uart)
     extron.subscribe()
