9b7b8d3e0f81a37df7e4104e10bbda1ceb7aed9a
Joe Baumgartner <3257771+joeybaumgartner@users.noreply.github.com>
2025-04-11 05:48:08 -0500

    Add JavaScript functions to dynamically add triggers. Add CSS to support form controls and start building out grid templates.

--- static/js/control_panel.js
+++ static/js/control_panel.js
@@ -1,55 +1,58 @@
+function getValueForId(id) {
+    return document.getElementById(id).value;
+}
+
 async function getControlPanelData() {
     const request = new Request('/control-panel-data');
 
     fetch(request)
     .then(response => {
-      if(!response.ok) {
-        throw new Error('Network response error');
-      }
-      return response.json();
+        if(!response.ok) {
+            throw new Error('Network response error');
+        }
+        return response.json();
     })
     .then(data => {
-      if(data) {
-        // Populate hotspot/SSID data
-        for(const [key, value] of Object.entries(data.ssid_info)) {
-          document.getElementById(key).innerHTML = value;
-        }
+        if(data) {
+            // Populate hotspot/SSID data
+            for(const [key, value] of Object.entries(data.ssid_info)) {
+                document.getElementById(key).innerHTML = value;
+            }
 
-        // Populate STA data
-        for(const [key, value] of Object.entries(data.sta)) {
-          document.getElementById(key).innerHTML = value;
-        }
+            // Populate STA data
+            for(const [key, value] of Object.entries(data.sta)) {
+                document.getElementById(key).innerHTML = value;
+            }
 
-        // Determine what displays on page
-        document.getElementById("disconnect-form").style.display = data.connected ? '' : 'none';
-        document.getElementById("sta-disconnect-form").style.display = data.connected ? '' : 'none';
+            // Determine what displays on page
+            document.getElementById("disconnect-form").style.display = data.connected ? '' : 'none';
+            document.getElementById("sta-disconnect-form").style.display = data.connected ? '' : 'none';
 
-        if(data.connected) {
-          if(data.saved_connection_exists && data.sta.saved_ssid != null) {
-            
-            if(data.ssid_info.saved_ssid == data.sta.sta_ssid) {
-              document.getElementById("delete-connection-form").style.display = '';
+            if(data.connected) {
+                if(data.saved_connection_exists && data.sta.saved_ssid != null) {
+                
+                    if(data.ssid_info.saved_ssid == data.sta.sta_ssid) {
+                        document.getElementById("delete-connection-form").style.display = '';
+                    }
+                    else {
+                        let owForm = document.getElementById("overwrite-connection-form");
+                        owForm.style.display = '';
+                        // add in ssid and password
+                        document.getElementById("delete-connection-form").style.display = '';
+                    }
             }
             else {
-              let owForm = document.getElementById("overwrite-connection-form");
-              owForm.style.display = '';
-              // add in ssid and password
-              document.getElementById("delete-connection-form").style.display = '';
+                document.getElementById("save-connection-form").style.display = '';
             }
-          }
-          else {
-            document.getElementById("save-connection-form").style.display = '';
-          }
+            }
+            else {
+                if(data.saved_connection_exists) {
+                    document.getElementById("delete-connection-form").style.display = '';
+                }
+            }
         }
-        else {
-          if(data.saved_connection_exists) {
-            document.getElementById("delete-connection-form").style.display = '';
-          }
-
-        }
-      }
     })
-  }
+}
 
 async function populateNetworks() {
     
@@ -60,33 +63,119 @@ async function populateNetworks() {
 
     fetch(request)
     .then(response => {
-      if(!response.ok) {
+        if(!response.ok) {
         throw new Error('Network response error');
-      }
-      return response.json();
+        }
+        return response.json();
     })
     .then(data => {
 
-      if (data) {
+        if (data) {
         // Data found, remove everything in list.
         selectList = document.getElementById("network-select");
         selectList.innerHTML = "";
         
         data.forEach(function(ssid) {
-          // Create new option for the SSID
-          let option = document.createElement("option");
-          option.value = ssid;
-          option.text = ssid;
-
-          // Add option to SSID list.
-          selectList.appendChild(option);
+            // Create new option for the SSID and add to list.
+            selectList.appendChild(createOption(ssid, ssid));
         });
-      }
+        }
     })
     .catch(error => {
-      console.error("Could not load JSON data");
+        console.error("Could not load JSON data");
     })
     .finally(() => {
-      message.style.display = 'none';
+        message.style.display = 'none';
     });
-  }
+}
+
+
+function addTrigger(t) {
+
+    var num = document.getElementsByClassName("trigger-count").length;
+
+    triggerRow = document.getElementById(t);
+
+    let name = document.createElement("input");
+    name.id = `trigger-${num}-name`;
+    name.type = "text";
+
+    triggerRow.appendChild(name);
+
+    let triggerInput = document.createElement("select");
+    triggerInput.id = `trigger-${num}-input`;
+    Array(6).keys().forEach(i => {
+        triggerInput.append(createOption(i + 1, `Input ${i + 1}`));
+    });
+
+    triggerRow.appendChild(triggerInput);
+
+    let triggerMode = document.createElement("select")
+    triggerMode.id = `trigger-${num}-mode`;
+    triggerMode.append(createOption("Remote", "Remote"));
+    triggerMode.append(createOption("SVS", "SVS"));
+    triggerRow.appendChild(triggerMode)
+
+    let triggerProfile = document.createElement("select");
+    triggerProfile.id = `trigger-${num}-profile`;
+    Array(12).keys().forEach(i => {
+        triggerProfile.appendChild(createOption(i + 1, `Profile ${i + 1}`));
+    });
+
+    triggerRow.appendChild(triggerProfile);
+}
+
+function createOption(value, text) {
+    let x = document.createElement("option");
+    x.value = value;
+    x.text = text
+
+    return x;
+}
+
+function toggle() {
+    let x = getValueForId("tcp-server-enabled");
+    let y = getValueForId("tcp-server-port");
+
+    y.disabled = !(x === "true");
+}
+
+function toggleSerialTelnet() {
+    var x = getValueForId("extron-connection");
+    
+    var s = getValueForId("extron-serial");
+    var t = getValueForId("extron-telnet");
+
+    if(x.value === "telnet") {
+        s.style.display = 'none';
+        t.style.display = '';
+    }
+    else {
+        s.style.display = '';
+        t.style.display = 'none';
+    }
+}
+
+function buildJson() {
+    tink = {
+        txPin: +getValueForId("tink-tx-pin"),
+        rxPin: +getValueForId("tink-rx-pin"),
+        uartId: +getValueForId("tink-uart")
+    }
+
+    tcpServer = {
+        enabled: +getValueForId("tcp-server-enabled") === "true",
+        port: +getValueForId("tcp-server-port")
+    }
+
+    switcher = {
+        enabled: +getValueForId("extron-")
+    }
+
+    j = {
+        tink: tink,
+        tcpServer: tcpServer
+    };
+
+    return j;
+}
